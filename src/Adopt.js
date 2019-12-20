e533365198c10d020ca89fb290f666e5dc16b4f7
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-12-20 19:59:23 +0000

    fixed Adopt componentdidmount

--- src/Adopt.js
+++ src/Adopt.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import Cat from './Cat';
 import Dog from './Dog';
 import Humans from './Humans';
+import petfulApi from './Util/petful-api';
 
 class Adopt extends Component {
+  state = {
+    humans: ''
+  }
+
+  componentDidMount() {
+    Promise.all([petfulApi.getHumans(), ])
+      .then(res => {
+        this.setState({humans: res[0]});
+      })
+  }
+  
   
   render(){
     return (
